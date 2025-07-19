2b7b42e630a855fc053c0df701d230c3233b3d1d
andyjxck <andyblewett9@gmail.com>
2025-07-19 17:43:02 +0100

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5409,7 +5409,7 @@ onClick={() => {
           </div>
         </div>
       )}
-   {showDomainsModal && (
+   {showWelcomeModal && (
   <div className="fixed inset-x-0 top-0 z-50 bg-black/60 flex justify-center p-4">
     <div
       className="
