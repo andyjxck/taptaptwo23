fb913d7629913e1ba9795fa637e43e5f905bf635
andyjxck <andyblewett9@gmail.com>
2025-07-19 21:58:11 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,24 +11,23 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <>
-      <html lang="en">
-        <head>
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
-          />
-        </head>
-        <body className={inter.className}>
-          <Script
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9540548227467393"
-            strategy="afterInteractive"
-            crossOrigin="anonymous"
-          />
-          {children}
-        </body>
-      </html>
-    </>
+    <html lang="en">
+      <head>
+        <meta name="google-adsense-account" content="ca-pub-9540548227467393" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+        />
+      </head>
+      <body className={inter.className}>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9540548227467393"
+          strategy="afterInteractive"
+          crossOrigin="anonymous"
+        />
+        {children}
+      </body>
+    </html>
   )
 }
