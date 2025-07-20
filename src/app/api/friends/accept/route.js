965eee14922e6d0c24ddb5c69ea4c86b9cf19063
andyjxck <andyblewett9@gmail.com>
2025-07-20 22:07:24 +0100

    Update route.js

--- src/app/api/friends/accept/route.js
+++ src/app/api/friends/accept/route.js
@@ -1,7 +1,9 @@
 // src/app/api/friends/accept/route.js
 
 import { NextResponse } from 'next/server';
-import db from '../../auth-handler/db'; // adjust if your db file path differs
+
+import db from '../../../auth-handler/db';
+
 
 export async function POST(req) {
   try {
