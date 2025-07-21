e6cf8f0fc6452ca4ea211636df571eb0b62c8b09
andyjxck <andyblewett9@gmail.com>
2025-07-21 22:49:42 +0100

    Update AdBanner.js

--- src/components/AdBanner.js
+++ src/components/AdBanner.js
@@ -1,6 +1,4 @@
-// src/components/AdBanner.js
-import Script from 'next/script'
-import { useEffect } from 'react'
+import { useEffect } from 'react';
 
 export default function AdBanner() {
   useEffect(() => {
@@ -12,19 +10,13 @@ export default function AdBanner() {
   }, []);
 
   return (
-    <>
-      <ins className="adsbygoogle"
-           style={{ display: 'block' }}
-           data-ad-client="ca-pub-9540548227467393"
-           data-ad-slot="9852599351"
-           data-ad-format="auto"
-           data-full-width-responsive="true"></ins>
-      <Script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9540548227467393"
-        strategy="afterInteractive"
-        crossOrigin="anonymous"
-      />
-    </>
+    <ins
+      className="adsbygoogle"
+      style={{ display: 'block' }}
+      data-ad-client="ca-pub-9540548227467393"
+      data-ad-slot="9852599351"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    />
   );
 }
