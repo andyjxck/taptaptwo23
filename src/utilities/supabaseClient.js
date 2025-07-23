468669438e9b6c38d4fb840622a324f6dbf8b536
andyjxck <andyblewett9@gmail.com>
2025-07-23 03:29:57 +0100

    Update supabaseClient.js

--- src/utilities/supabaseClient.js
+++ src/utilities/supabaseClient.js
@@ -1,5 +1,8 @@
-
 import { createClient } from '@supabase/supabase-js'
+
 const supabaseUrl = 'https://twkxrgkkfhaylroyhfmi.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+
+// Use the anon/public key environment variable here, make sure you set it in your environment (.env.local or similar)
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
