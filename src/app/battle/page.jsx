1e6f4ab85e628277d619022500e438348b724aad
andyjxck <andyblewett9@gmail.com>
2025-07-24 21:52:43 +0100

    Update page.jsx

--- src/app/battle/page.jsx
+++ src/app/battle/page.jsx
@@ -1318,7 +1318,7 @@ if (gamePhase === "playing") {
         </div>
 
         {/* Scoreboard below top buttons */}
-        <div className="z-10 mt-14 max-w-md mx-auto text-white select-none space-y-3 w-full">
+        <div className="z-10 mt-25 max-w-md mx-auto text-white select-none space-y-3 w-full">
           {/* Scoreboard - horizontal layout */}
           <div className="backdrop-blur-xl bg-white/10 border border-white/20 p-2 shadow-xl rounded-xl flex justify-between items-center text-xs sm:text-sm font-bold">
             {/* Player 1 */}
