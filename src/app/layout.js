49571d22ad646bd254c3f6d3a098097ec4195341
andyjxck <andyblewett9@gmail.com>
2025-08-02 00:41:21 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,17 +17,6 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
         />
-        {/* Gatekeeper Consent scripts */}
-        <Script
-          src="https://cmp.gatekeeperconsent.com/min.js"
-          data-cfasync="false"
-          strategy="afterInteractive"
-        />
-        <Script
-          src="https://the.gatekeeperconsent.com/cmp.min.js"
-          data-cfasync="false"
-          strategy="afterInteractive"
-        />
         {/* Ezoic script */}
         <Script
           async
