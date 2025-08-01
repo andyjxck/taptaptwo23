fcf7b84b1b8d8342e39a0dc2c19270d4c099b933
andyjxck <andyblewett9@gmail.com>
2025-08-02 00:17:11 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,11 +13,33 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* Remove Google AdSense meta tag and script */}
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
         />
+        {/* Gatekeeper Consent scripts */}
+        <Script
+          src="https://cmp.gatekeeperconsent.com/min.js"
+          data-cfasync="false"
+          strategy="afterInteractive"
+        />
+        <Script
+          src="https://the.gatekeeperconsent.com/cmp.min.js"
+          data-cfasync="false"
+          strategy="afterInteractive"
+        />
+        {/* Ezoic script */}
+        <Script
+          async
+          src="//www.ezojs.com/ezoic/sa.min.js"
+          strategy="afterInteractive"
+        />
+        <Script id="ezstandalone-init" strategy="afterInteractive">
+          {`
+            window.ezstandalone = window.ezstandalone || {};
+            ezstandalone.cmd = ezstandalone.cmd || [];
+          `}
+        </Script>
       </head>
       <body className={inter.className}>
         {children}
