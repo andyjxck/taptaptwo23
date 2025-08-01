f8f4d82f3acb3f70af6cca08fbdf5e5d1b3bfa67
andyjxck <andyblewett9@gmail.com>
2025-08-01 19:17:23 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,16 +13,12 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta name="google-adsense-account" content="ca-pub-9540548227467393" />
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9540548227467393"
      crossorigin="anonymous"></script>
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
         />
       </head>
-      <body className={inter.className}>
-        {children}
-      </body>
     </html>
   )
 }
